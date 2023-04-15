ec0e7f3f0d2480db148b5d2a7c01c244b7593ebb
Crowdbotics <team@crowdbotics.com>
2023-04-15 01:38:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "torneosapp-39953",
+  url: "https://torneosapp-39953.botics.co",
+  api: "https://torneosapp-39953.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
